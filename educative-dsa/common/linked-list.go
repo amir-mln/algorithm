8cb30f5575e0c2c068380e48afc351ea20ef1b64
amir-mln <molaeyan.career@gmail.com>
2024-07-07 02:21:22 +0330

    create `common` pkg for common ds and algorithms

---
+++ educative-dsa/common/linked-list.go
@@ -0,0 +1,44 @@
+package common
+
+import "fmt"
+
+type LinkedList struct {
+	Head *LinkedListNode
+}
+
+func (l *LinkedList) String() string {
+	s := make([]int, 0)
+	c := l.Head
+
+	for c != nil {
+		s = append(s, c.Value)
+		c = c.Next
+	}
+
+	return fmt.Sprint(s)
+}
+
+func (l *LinkedList) InsertNodeAtHead(node *LinkedListNode) {
+	if l.Head == nil {
+		l.Head = node
+	} else {
+		node.Next = l.Head
+		l.Head = node
+	}
+}
+
+func (l *LinkedList) InsertFromList(lst []int) {
+	for i := len(lst) - 1; i >= 0; i-- {
+		newNode := &LinkedListNode{
+			Value: lst[i],
+			Next:  nil,
+		}
+
+		l.InsertNodeAtHead(newNode)
+	}
+}
+
+type LinkedListNode struct {
+	Value int
+	Next  *LinkedListNode
+}
